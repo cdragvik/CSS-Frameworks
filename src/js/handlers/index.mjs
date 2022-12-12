a3270bdf78d8992839191d9110f29310a29c8c5a
Cdragvik <cndragvik@gmail.com>
2022-12-12 13:37:59 +0100

    templating for posts

---
+++ src/js/handlers/index.mjs
@@ -0,0 +1,4 @@
+export * from "./createPost.mjs";
+export * from "./login.mjs";
+export * from "./signup.mjs";
+export * from "./updatePost.mjs";
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,13 +1,26 @@
-import { setRegisterFormListener } from "./handlers/signup.mjs";
-import { setLoginFormListener } from "./handlers/login.mjs";
-import * as post from "./api/posts/index.mjs";
+
+import * as listeners from "./handlers/index.mjs"
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
 
-setRegisterFormListener()
-setLoginFormListener()
+listeners.setRegisterFormListener()
+listeners.setLoginFormListener()
+listeners.setCreateFormListener()
+listeners.setUpdateFormListener()
 
 // post.createPost()
 // post.updatePost()
 // post.removePost()
 // post.getPost()
 // post.getPosts()
+
+
+// async function testTemplate() {
+//     const posts = await postMethods.getPosts();
+//     const post = posts.pop()
+//     const container = document.querySelector("#post");
+//     templates.renderPostTemplates(posts, container);
+// }
+
+// testTemplate()
