f4e1c58a7559ef0c436bdca1dc3b661d8b046258
Cdragvik <cndragvik@gmail.com>
2022-12-13 19:18:42 +0100

    changed header

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -2,14 +2,16 @@ export function postTemplate(postData) {
     const post = document.createElement("div");
     post.classList.add("post");
     post.innerHTML =  `<div class="card p-2 mb-2" id=${postData.id}>
-        <p class="text-muted">${postData.name}</p>
+        <p class="text-muted">${postData.author}</p>
         <div>
-        <h2>${postData.title}</h2>
-        <p>${postData.body}</p>
-        <div class="d-flex text-muted">       
-        <p class="me-3">Likes: ${postData._count.reactions}</p>
-        <p>Comments: ${postData._count.comments}</p>
-        </div></div>`
+            <h2>${postData.title}</h2>
+            <p>${postData.body}</p>
+            <a href="/post/index.html?id=${postData.id}"<button class="btn btn-light">View more</button></a>
+                <div class="d-flex text-muted pt-4">       
+                    <p class="me-3">Likes: ${postData._count.reactions}</p>
+                    <p>Comments: ${postData._count.comments}</p>
+                </div>
+        </div>`
     return post;
 }
 
